99c6c22fae0f3801f791818c532502fb8216a6b8
PBBassily <paulaboules@pharos-mac-sever.local>
2018-10-02 11:49:41 +0200

    dreams tableViews and favCreature tableView

---
+++ LucidDreams/LucidTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  LucidTableViewCell.swift
+//  LucidDreams
+//
+//  Created by Paula Boules on 10/2/18.
+//  Copyright © 2018 Paula Boules. All rights reserved.
+//
+
+import UIKit
+
+class LucidTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBOutlet weak var lucidLabel: UILabel!
+    @IBOutlet weak var lucidImageView: UIImageView!
+    
+    
+    
+}
---
+++ LucidDreams/Supporting Files/LucidTableViewController.swift
@@ -0,0 +1,132 @@
+//
+//  LucidTableViewController.swift
+//  LucidDreams
+//
+//  Created by Paula Boules on 10/2/18.
+//  Copyright © 2018 Paula Boules. All rights reserved.
+//
+
+import UIKit
+
+
+
+class LucidTableViewController: UITableViewController, FavCreatureTableViewDelegate{
+    var favCreatureName = "Creature"
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0 : return 1
+        case 1 : return 3
+        default : return 0
+        }
+        
+    }
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Configure the cell...
+        let cell : LucidTableViewCell
+        if indexPath.section == 0 {
+            
+            cell = tableView.dequeueReusableCell(withIdentifier: "FavCreatureCell", for: indexPath) as! LucidTableViewCell
+            cell.lucidLabel.text = favCreatureName
+        }
+        else {
+            cell = tableView.dequeueReusableCell(withIdentifier: "LucidDreamCell", for: indexPath) as! LucidTableViewCell
+            cell.lucidLabel.text = "Dream"
+            cell.accessoryType  = .disclosureIndicator
+        }
+        
+        
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0 : return "FAVORITE CREATURE"
+        case 1 : return "DREAMS"
+        default : return nil
+        }
+    }
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    //MARK: - Delegation
+    
+    func favCreatureDidSelected(with name: String) {
+        favCreatureName = name
+        tableView.reloadData()
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
